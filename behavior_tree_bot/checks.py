7ba6422513020bde30390eab647c69df382a0e5e
Darcy Phipps <diphipps@ucsc.edu>
2020-10-30 17:33:27 -0700

    Added check for defensive purposes

--- behavior_tree_bot/checks.py
+++ behavior_tree_bot/checks.py
@@ -9,3 +9,8 @@ def have_largest_fleet(state):
              + sum(fleet.num_ships for fleet in state.my_fleets()) \
            > sum(planet.num_ships for planet in state.enemy_planets()) \
              + sum(fleet.num_ships for fleet in state.enemy_fleets())
+
+def is_enemy_fleet_incoming(state):
+    # Store the list of planets in a variable to prevent this algorithm from being too inefficient
+    my_planets = state.my_planets()
+    return any(fleet.destination_planet in my_planets for fleet in state.enemy_fleets())
